8960b13ceff5bfa99f61b555ea1d1ede54bfc40a
EML <emiliano.lupo@henn.com>
2024-06-17 09:51:59 +0200

    initial commit

---
+++ src/mysql-wrap.py
@@ -0,0 +1,7 @@
+import mysql.connector as mysql
+from collections import namedtuple
+from itertools import repeat
+from typing import Any, Optional, Set, Callable, Iterator
+
+import pandas as pd
+import numpy as np
